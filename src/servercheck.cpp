b85bb3360fdf1ce941a8873dd92b4274e085d9d3
Chase 李 <clee231@uic.edu>
2018-02-17 16:08:46 -0600

    Preliminary fetching of md5 sums from the server.

--- src/servercheck.cpp
+++ src/servercheck.cpp
@@ -3,15 +3,29 @@
 
 namespace GPEUP {
 	
+	int ServerCheck::getVersion(CURL* curl) {
+
+		return 0;
+	}
+
+	size_t ServerCheck::read_data(void *ptr, size_t size, size_t nmemb, void *stream)
+	{
+		((std::string*)stream)->append((char*)ptr, size * nmemb);
+		return size * nmemb;
+	}
+
 	int ServerCheck::check() {
 		CURL *curl;
 		CURLcode res;
+		std::string manifest;
 
 		curl_global_init(CURL_GLOBAL_DEFAULT);
 
 		curl = curl_easy_init();
 		if (curl) {
 			curl_easy_setopt(curl, CURLOPT_URL, GPEU_RELEASE_URL);
+			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, read_data);
+			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &manifest);
 
 			res = curl_easy_perform(curl);
 
@@ -19,7 +33,7 @@ namespace GPEUP {
 				std::cerr << "Unable to perform request" << curl_easy_strerror(res) << std::endl;
 			}
 			else {
-				std::cout << curl << std::endl;
+				md5LineParse(manifest);
 			}
 			curl_easy_cleanup(curl);
 		}
@@ -27,6 +41,25 @@ namespace GPEUP {
 		curl_global_cleanup();
 
 		return 0;
+	}
+
+	bool ServerCheck::md5LineParse(const std::string input) {
+		;
+		std::regex md5Regex("[a-f0-9]{32}", std::regex::ECMAScript | std::regex::icase);
+
+		std::sregex_iterator md5Begin = std::sregex_iterator(input.begin(), input.end(), md5Regex);
+		std::sregex_iterator md5End = std::sregex_iterator();
+
+		std::cout << "Found " << std::distance(md5Begin, md5End) << " words:\n";
+
+		for (std::sregex_iterator i = md5Begin; i != md5End; ++i) {
+			std::smatch match = *i;
+			std::string match_str = match.str();
+			std::cout << match_str << '\n';
+		}
+		
+
+		return false;
 	}
 
 }
--- src/servercheck.hpp
+++ src/servercheck.hpp
@@ -1,6 +1,8 @@
 // updater.hpp
 #include <iostream>
 #include <string>
+#include <regex>
+#include <map>
 #include <curl/curl.h>
 #include "updaterConfig.hpp"
 
@@ -11,11 +13,13 @@ class ServerCheck
 {
 private:
 	int getVersion(CURL* servdata);
+	static size_t read_data(void *ptr, size_t size, size_t nmemb, void *stream);
 public:
     int check();
 
     std::string config;
 	std::string serverVersion;
+	bool md5LineParse(std::string input);
 };
 
 }
